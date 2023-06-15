e0d1b42de727ac532c315097b9f8a93452c53d63
JuliaFRibeiro <83432455+JuliaFRibeiro@users.noreply.github.com>
2023-06-15 13:51:59 -0300

    jogo
    finalizado

---
+++ Assets/Scripts/Controle.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controle : MonoBehaviour
+{
+    public FloatingJoystick moveJoystick;
+    public Rigidbody2D oRigidbody2D;
+    private float moveH, moveV, speedMove = 5;
+
+    public GameObject laser;
+    public Transform LaserSpaw;
+
+    public float VelocidadeDaNave;
+
+    public float timeMaxEntreLaser;
+    public float timeAtualLaser;
+
+    public bool playerAtirador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovimentarJogador();
+
+        if (playerAtirador == true)
+        {
+            AtirarLaiser();
+        }
+    }
+
+    private void MovimentarJogador()
+    {
+        moveH = moveJoystick.Horizontal;
+        moveV = moveJoystick.Vertical;
+        Vector2 dir = new Vector2(moveH, moveV);
+        oRigidbody2D.velocity = new Vector3(moveH * speedMove, oRigidbody2D.velocity.y, moveV * speedMove);
+        if (dir != Vector2.zero)
+        {
+            transform.LookAt(transform.position);
+        }
+    }
+
+    private void AtirarLaiser()
+    {
+        timeAtualLaser -= Time.deltaTime;
+
+        if (timeAtualLaser <= 0)
+        {
+            Instantiate(laser, LaserSpaw.position, Quaternion.Euler(0f, 0f, 0f));
+            timeAtualLaser = timeMaxEntreLaser;
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject pnlGameOver;
+    public GameObject pnlmouse;
+    
+    public TMP_Text txtLives;
+
+   
+
+    public GameObject Player;
+
+
+    
+    int Lives = 3;
+
+    public static UIManager instance;
+
+    private void Start()
+    {
+        txtLives.text = "3";
+    }
+
+   
+
+    public void SetSLives(int value)
+    {
+        Lives -= value;
+        txtLives.text = Lives.ToString();
+
+        if (Lives <= 0)
+        {
+            Destroy(Player.gameObject);
+            GameOver();
+        }
+    }
+
+    public void MyLoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void GameOver()
+    {
+        pnlGameOver.SetActive(true);
+        pnlmouse.SetActive(false);
+    }
+
+}
